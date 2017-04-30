148d01a8a2a0c0af9939004153d4f78dc8a2d003
fanyuwen <18762677070@163.com>
2017-04-30 11:38:20 +0800

    this is my first github project

---
+++ src/main/java/com/fanyuwen/Test.java
@@ -0,0 +1,10 @@
+package com.fanyuwen;
+
+/**
+ * Created by asus on 2017/4/30.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
