633e5d0b8d861f4765dc706581d05f9bda245baa
fanyuwen <18762677070@163.com>
2017-05-08 01:20:35 +0800

    java nio test

---
+++ src/main/java/com/fanyuwen/javanio/socketChannel/server/ChargenServer.java
@@ -0,0 +1,109 @@
+package com.fanyuwen.javanio.socketChannel.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 创建一个java nio ServerSocketChannel实例
+ * Created by fanyuwen on 2017/5/7.
+ */
+public class ChargenServer {
+    private static int DEFAULT_PORT = 19;
+
+    public static void main(String[] args) {
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (RuntimeException ex) {
+            port = DEFAULT_PORT;
+        }
+
+        System.out.println("Listening for connections on port " + port);
+
+        byte[] rotation = new byte[95 * 2];
+        for (byte i = ' '; i <= '~'; i++) {
+            rotation[i - ' '] = i;
+            rotation[i + 95 - ' '] = i;
+        }
+
+        ServerSocketChannel serverChannel;
+        Selector selector;
+        try {
+            serverChannel = ServerSocketChannel.open();
+            ServerSocket ss = serverChannel.socket();
+            InetSocketAddress address = new InetSocketAddress(port);
+            ss.bind(address);
+            serverChannel.configureBlocking(false);
+            selector = Selector.open();
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        while (true) {
+            try {
+                selector.select();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+            Set<SelectionKey> readKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        System.out.println("Accepted connection form " + client);
+                        client.configureBlocking(false);
+                        SelectionKey key2 = client.register(selector, SelectionKey.OP_WRITE);
+                        ByteBuffer buffer = ByteBuffer.allocate(74);
+                        buffer.put(rotation, 0, 72);
+                        buffer.put((byte) '\r');
+                        buffer.put((byte) '\n');
+                        buffer.flip();
+                        key2.attach(buffer);
+                    } else if (key.isWritable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
+                        if (!buffer.hasRemaining()) {
+                            //用下一行重新填充缓冲区
+                            buffer.rewind();
+                            //得到上一次的首字符
+                            int first = buffer.get();
+                            //准备改变缓冲区中的数据
+                            buffer.rewind();
+                            //寻找rotation中新的首字符位置
+                            int position = first - ' ' + 1;
+                            //将数据从rotation复制到缓冲区
+                            buffer.put(rotation, position, 72);
+                            //在缓冲区末尾存储一个行分隔符
+                            buffer.put((byte) '\r');
+                            buffer.put((byte) '\n');
+                            //准备缓冲区进行写入
+                            buffer.flip();
+                        }
+                        client.write(buffer);
+                    }
+                } catch (IOException e) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException e1) {
+                    }
+                }
+            }
+        }
+    }
+}
