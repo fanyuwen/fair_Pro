b6e8f36da8da11ff8b56adbb52557ff31d4a1b28
fanyuwen <18762677070@163.com>
2017-07-29 18:33:13 +0800

    create the map and list build method 1.0.0

--- src/main/java/com/fanyuwen/Test.java
+++ src/main/java/com/fanyuwen/Test.java
@@ -1,12 +1,10 @@
 package com.fanyuwen;
 
-import java.net.URI;
-
 /**
  * Created by asus on 2017/4/30.
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!!");
+
     }
 }
---
+++ src/main/java/com/fanyuwen/util/CollectionUtil.java
@@ -0,0 +1,163 @@
+package com.fanyuwen.util;
+
+import java.util.*;
+
+public class CollectionUtil {
+
+    public static <K, V> MapBuilder<K, V> getMapBuilder(K key, V value) {
+        return new MapBuilder<>(null, key, value);
+    }
+
+    public static <T> ListBuilder<T> getListBuilder(T value) {
+        return new ListBuilder<>(null, value);
+    }
+
+
+    public static class ListBuilder<T> extends AbstractBuilder<List<T>> {
+        private T value;
+        private ListBuilder<T> buildLink;
+        private ListBuilder<T> preBuildLink;
+
+        public ListBuilder(ListBuilder<T> listBuilder, T value) {
+            Objects.requireNonNull(value);
+            if (listBuilder != null)
+                listBuilder.buildLink = this;
+            this.preBuildLink = listBuilder;
+            this.value = value;
+        }
+
+        public ListBuilder<T> add(T value) {
+            return new ListBuilder<>(this, value);
+        }
+
+        public ListBuilder<T> add(T value, T... values) {
+            ListBuilder<T> listBuilder = add(value);
+            for (T t : values)
+                listBuilder = listBuilder.add(t);
+            return listBuilder;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        @Override
+        public ListBuilder<T> getBuildLink() {
+            return buildLink;
+        }
+
+        @Override
+        public ListBuilder<T> getPreBuildLink() {
+            return preBuildLink;
+        }
+
+        @Override
+        public ListBuilder<T> revert() {
+            return (ListBuilder<T>) super.revert();
+        }
+
+        @Override
+        public List<T> build() {
+            return build(ArrayList::new);
+        }
+
+        @Override
+        public List<T> build(Generate<List<T>> generate) {
+            List<T> list = generate.generate();
+            ListBuilder<T> listBuilder = revert();
+            while (listBuilder != null) {
+                list.add(listBuilder.getValue());
+                listBuilder = listBuilder.getBuildLink();
+            }
+            return list;
+        }
+
+    }
+
+    public static class MapBuilder<K, V> extends AbstractBuilder<Map<K, V>> {
+        private K key;
+        private V value;
+
+        private MapBuilder<K, V> buildLink;
+        private MapBuilder<K, V> preBuildLink;
+
+        public MapBuilder(MapBuilder<K, V> buildLink, K key, V value) {
+            Objects.requireNonNull(key);
+            Objects.requireNonNull(value);
+            if (buildLink != null)
+                buildLink.buildLink = this;
+            this.preBuildLink = buildLink;
+            this.key = key;
+            this.value = value;
+        }
+
+        public MapBuilder<K, V> add(K key, V value) {
+            return new MapBuilder<>(this, key, value);
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public MapBuilder<K, V> getBuildLink() {
+            return buildLink;
+        }
+
+        @Override
+        public Builder<Map<K, V>> getPreBuildLink() {
+            return preBuildLink;
+        }
+
+        @Override
+        public MapBuilder<K, V> revert() {
+            return (MapBuilder<K, V>) super.revert();
+        }
+
+        @Override
+        public Map<K, V> build() {
+            return build(HashMap::new);
+        }
+
+        @Override
+        public Map<K, V> build(Generate<Map<K, V>> generate) {
+            Map<K, V> map = generate.generate();
+            MapBuilder<K, V> builder = revert();
+            while (builder != null) {
+                map.put(builder.getKey(), builder.getValue());
+                builder = builder.getBuildLink();
+            }
+            return map;
+        }
+    }
+
+    private interface Builder<T> {
+        T build();
+
+        T build(Generate<T> generate);
+
+        Builder<T> getBuildLink();
+
+        Builder<T> getPreBuildLink();
+
+        Builder<T> revert();
+
+        interface Generate<T> {
+            T generate();
+        }
+    }
+
+    private static abstract class AbstractBuilder<T> implements Builder<T> {
+        @Override
+        public Builder<T> revert() {
+            Builder<T> preBuilder = this;
+            while (preBuilder.getPreBuildLink() != null)
+                preBuilder = preBuilder.getPreBuildLink();
+            return preBuilder;
+        }
+    }
+
+}
