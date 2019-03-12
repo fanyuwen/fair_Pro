b6bcfae81be8bbc3663b7273a2e08ab8dbb5f051
fanyuwen <123456789>
2019-03-12 17:38:32 +0800

    位操作类添加注释

--- src/main/java/com/fanyuwen/bitoperation/BitOperate.java
+++ src/main/java/com/fanyuwen/bitoperation/BitOperate.java
@@ -1,9 +1,10 @@
 package com.fanyuwen.bitoperation;
 
+/**
+ * @author 樊宇雯 参考《算法心得》 Henry S.Warren, Jr.
+ */
 public class BitOperate {
     private BitOperate() {
         throw new RuntimeException("this class " + BitOperate.class + " can't be create.");
     }
-
-
 }
